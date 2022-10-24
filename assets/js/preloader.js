624acc54adfc11cd202870c2269e496aa02c392e
Sithumameesha <sithumameesha76@gmail.com>
2022-10-24 09:58:16 +0530

    Set Pre Loader

---
+++ assets/js/preloader.js
@@ -0,0 +1,15 @@
+// const loader = document.querySelector('.Preloader');
+//
+// window.addEventListener('load', () => {
+//
+//     // Making 5s delay in website loading
+//     setTimeout(() => {
+//
+//         loader.classList.add('fadeOut');
+//         setTimeout(() => {
+//             loader.style.display = 'none';
+//         }, 1000);
+//
+//     }, 5000);
+//
+// });
