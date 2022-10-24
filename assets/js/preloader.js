6dbd922f4d59373ac6dae65515c9c2abd9e44b85
Sithumameesha <sithumameesha76@gmail.com>
2022-10-24 10:13:42 +0530

    Add Preloader

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,15 +1,3 @@
-// const loader = document.querySelector('.Preloader');
-//
-// window.addEventListener('load', () => {
-//
-//     // Making 5s delay in website loading
-//     setTimeout(() => {
-//
-//         loader.classList.add('fadeOut');
-//         setTimeout(() => {
-//             loader.style.display = 'none';
-//         }, 1000);
-//
-//     }, 5000);
-//
-// });
+$(window).on('load',function (){
+    $(".Preloader").fadeOut(10000);
+})
